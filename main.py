81c3c1fe31688265575a38e6e38c8f11c6eb2473
AlkBur <alkboor@mail.ru>
2016-05-06 16:25:36 +0300

    Added changes

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import socket
 import sys
- 
+
 # Create a TCP/IP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address given on the command line
 server_address = ('0.0.0.0', 2222)
-s.bind(server_address)
+sock.bind(server_address)
 print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
-s.listen(1)
+sock.listen(1)
 
 while True:
-	print >>sys.stderr, 'waiting for a connection'
-	client, address = s.accept()
-while True:
-data = client.recv(1024)
-if not data: break
-if data == "Close" or data == "close": client.close()
-client.send(data)
-client.close()
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'client connected:', client_address
+        while True:
+            data = connection.recv(1024)
+            print >>sys.stderr, 'received "%s"' % data
+            if not data:
+                break
+            if data == "Close" or data == "close":
+                connection.close()
+            else:
+                connection.send(data)
+    finally:
+        connection.close()
